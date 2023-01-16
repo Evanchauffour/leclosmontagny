b07bf1e9f6b2594a513fb5f2f9137ae97faa743f
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2023-01-16 15:35:51 +0100

    home page v1 no responsive

--- front-page.php
+++ front-page.php
@@ -4,6 +4,9 @@
 
     // Houses
     $houses = get_field('houses');
+
+    // About us
+    $about_us = get_field('about_us');
 ?>
 
 <div class="home">
@@ -15,8 +18,9 @@
 <div class="container-infos-gites">
 <?php 
     foreach($houses as $house):
-    $image = $house['image']
+    $image = $house['image'];
 ?>
+
         <div class="info-gite">
             <img src="<?php echo($house['image']['url']); ?>" height="" width="" alt="<?php echo esc_attr($image['alt']);?>">
             <div class="text-info-gite">
@@ -27,47 +31,36 @@
         </div>
 <?php endforeach; ?>
 </div>
-    <!-- <div class="info-gite nivolet">
-        <img src="http://127.0.0.1/leclosmontagny/wp-content/themes/leclosmontagny/assets/img/minia-pavillon-nivolet.jpg" alt="">
-        <div class="text-info-gite">
-            <h3>LE PAVILLON<br> DU <span>Nivolet</span></h3>
-            <p></p>
-            <a href="">DÉCOUVRIR</a>
-        </div>
-    </div>
-    <div class="info-gite">
-            <img src="http://127.0.0.1/leclosmontagny/wp-content/themes/leclosmontagny/assets/img/minia-pavillon-nivolet.jpg" alt="">
-            <div class="text-info-gite">
-            <h3>LE PAVILLON<br> DU <span>Granier</span></h3>
-            <p></p>
-            <a href="">DÉCOUVRIR</a>
-        </div>
-    </div> -->
-</div>
 <div class="container-about_us">
     <h1 class="title-home">En <span>famille</span>, entre <span>amis</span> ou pour un événement <span>professionnel</span>, nous créons le fil de votre lien...</h1>
     <div class="container-video-about_us">
         <div class="text-info-video-about_us">
             <h3>DÉCOUVREZ <br>UN<span>Charme</span></h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo odio magni ullam, doloribus non necessitatibus asperiores corporis. Quod quia vero ipsum doloremque ullam. Ratione facere cum et, placeat similique quae!</p>
+            <p><?php the_field('description') ?></p>
         </div>
         <img src="http://127.0.0.1/leclosmontagny/wp-content/themes/leclosmontagny/assets/img/video-aboutus.png" alt="">
     </div>
     <div class="container-citation">
+<?php 
+    foreach($about_us as $about_me):
+    $image_about_me = $house['image']['url'];
+?>
         <div class="citation">
-            <img src="http://127.0.0.1/leclosmontagny/wp-content/themes/leclosmontagny/assets/img/video-aboutus.png" alt="">
+            <img src="<?php echo($about_me['image']['url']); ?>" alt="">
             <div class="text-citation">
-                <h3>Florence</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto rem adipisci animi doloribus possimus omnis vel quibusdam earum reiciendis suscipit?</p>
+                <h3><?php echo($about_me['name']);?></h3>
+                <p><?php echo($about_me['description']);?></p>
             </div>
         </div>
-        <div class="citation second-citation">
+
+<?php endforeach; ?>
+        <!-- <div class="citation second-citation">
                 <img src="http://127.0.0.1/leclosmontagny/wp-content/themes/leclosmontagny/assets/img/video-aboutus.png" alt="">
             <div class="text-citation">
                 <h3>Benôit</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto rem adipisci animi doloribus possimus omnis vel quibusdam earum reiciendis suscipit?</p>
             </div>
-        </div>
+        </div> -->
     </div>
     <div class="back-to-top"></div>
 </div>
