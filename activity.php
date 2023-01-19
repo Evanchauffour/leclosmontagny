18d41a3e96d719bd9444cb0066785bf69e0e406e
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2023-01-19 10:27:21 +0100

    grid activity

--- activity.php
+++ activity.php
@@ -2,5 +2,27 @@
     // Header
     get_header();
 
-    //Template name: Contact 
+    //Activities
+    $activities = get_field('activities');
+
+    //Template name: Activity 
 ?>
+
+<div class="container-activity">
+    <h1>Activités</h1>
+
+<div class="grid">
+<?php 
+    foreach($activities as $activity):
+?>
+    <div class="grid-item">
+        <img src="<?php echo($activity['image']['url']) ?>" alt="">
+        <div class="text">
+            <h2><?php echo($activity['title']) ?></h2>
+            <p><?php echo($activity['info']) ?></p>
+        </div>
+    </div>
+<?php endforeach; ?>
+</div>
+    <script src="<?php echo(get_template_directory_uri()); ?>/assets/js/activity.js"></script>
+</div>
