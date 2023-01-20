df8dbbcae38045e103d885e00ae13288558bc43f
Evanchauffour <111223072+Evanchauffour@users.noreply.github.com>
2023-01-20 20:30:48 +0100

    vf home responsive and footer responsive

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <img src="<?php echo(get_template_directory_uri()); ?>/assets/img/logo.svg" alt="">
+    <a href="<?php echo get_home_url();?>"><img src="<?php echo(get_template_directory_uri()); ?>/assets/img/logo.svg" alt=""></a>
     <a href="mentions-legales">Mentions légales</a>
     <p>©2023 - Le Clos Montagny</p>
     <script src="<?php echo(get_template_directory_uri()); ?>/assets/js/header.js"></script>
